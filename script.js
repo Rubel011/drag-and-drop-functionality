d708de908d9dbf88d6bd4446e39ec84e40257448
Rubel Foridi <rubelforidi@gmail.com>
2023-08-11 11:23:33 +0530

    add all files and complete project

---
+++ script.js
@@ -0,0 +1,26 @@
+const lists = document.getElementsByClassName("list");
+let rightBox = document.getElementById("right");
+let leftBox = document.getElementById("left");
+
+for (const list of lists) {
+    list.addEventListener("dragstart", (e) => {
+        let selected = e.target;
+
+        //for rignt box
+        rightBox.addEventListener("dragover", (e) => {
+            e.preventDefault();
+        });
+        rightBox.addEventListener("drop", (e) => {
+            rightBox.appendChild(selected);
+            selected = null;
+        }); //
+
+        leftBox.addEventListener("dragover", (e) => {
+            e.preventDefault();
+        });
+        leftBox.addEventListener("drop", (e) => {
+            leftBox.appendChild(selected);
+            selected = null;
+        }); //
+    });
+}
